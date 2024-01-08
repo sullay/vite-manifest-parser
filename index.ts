c85849501e016790751ab8c60f867e18a7765417
sullayang(杨金伟) <sullayang@futunn.com>
2024-01-08 19:45:07 +0800

    feat: add readme

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import path from "path";
+
 /**
- * 解析manifest.json文件，生成对应的代码块，方便后端集成使用
- * @param input input 入口文件，客户端项目的相对路径(manifest.json中入口文件的key值), 例如: src/app/main.ts
- * @param outDir 打包目录，请使用绝对路径 例如：path.resolve(__dirname, './app/public/dist')
- * @param publicPath 资源路径，例如：/public/dist/、 https://static.xxx.com/, 默认值 '/'
- * @param isDev 是否为开发环境，开发环境不会解析manifest.json文件，直接使用vite开发服务器，入口为 publicPath + input
+ * Parses the manifest.json file to generate corresponding code blocks for easy backend integration.
+ * In the development environment, the manifest.json file will not be parsed, and the entry will use the Vite development server directly at publicPath + input.
+ * @param input The entry file, which is the relative path in the client project (the key value in the manifest.json), for example: src/app/main.ts
+ * @param outDir The build directory, please use an absolute path, for example: path.resolve(__dirname, './app/public/dist')
+ * @param publicPath The resource path, such as: /public/dist/, https://static.xxx.com/; the default value is '/'
+ * @param isDev Indicates whether it is a development environment
  */
 export const parser = async ({
     input,
@@ -17,9 +19,9 @@ export const parser = async ({
   publicPath?: string,
   isDev?: boolean
 }) => {
-    if (!input || !publicPath) throw Error("参数有误");
+    if (!input || !publicPath) throw Error("Invalid parameters");
 
-    // 没有打包，css不需要提取，前端资源通过vite开发服务器解释执行
+    // When not bundled, CSS is not extracted, and frontend resources are served through the Vite development server.
     if (isDev) {
         return {
             preload: "",
@@ -30,20 +32,25 @@ export const parser = async ({
 
     let manifest = {};
     try {
+        // Attempt to dynamically import the manifest.json file
         manifest = await import(path.resolve(outDir, "./manifest.json"));
     } catch (error) {
-        console.error("打包环境，未找到manifest.json文件");
+        // If the manifest.json file is not found in the production environment, log an error and throw
+        console.error("In the production environment, the manifest.json file was not found");
         throw error;
     }
 
-    if (!manifest[input]) throw Error("manifest.json文件中未发现入口");
+    // If the entry specified in the manifest.json file is not found, throw an error
+    if (!manifest[input]) throw Error("Entry not found in the manifest.json file");
 
     const pageInfo = manifest[input];
+    // Attempt to retrieve legacy pageInfo for older browsers
     const legacyPageInfo = manifest[input.replace(/\.jsx?$|\.tsx?$/, "-legacy$&")];
+    // Retrieve information for legacy polyfills if available
     const legacyPolyfillsInfo = manifest["vite/legacy-polyfills-legacy"];
-    // 生成css link
+    // Generate HTML for CSS links
     const cssHtml = pageInfo.css.map(cssUrl => `<link rel="stylesheet" href="${publicPath}${cssUrl}" />`).join(" ");
-    // 生成preload link
+    // Generate HTML for preload links
     const preloadList = [input];
     if (Array.isArray(pageInfo.imports)) {
         preloadList.push(...pageInfo.imports);
@@ -53,6 +60,7 @@ export const parser = async ({
         return `<link rel="modulepreload" href="${publicPath}${manifest[pre].file}" />`;
     }).join(" ");
 
+    // If legacy fileInfo is not available, return the standard script tags
     if (!legacyPageInfo?.file || !legacyPolyfillsInfo?.file) {
         return {
             preload: preloadHtml,
@@ -60,9 +68,10 @@ export const parser = async ({
             js: `<script type="module" src="${publicPath}${pageInfo.file}"></script>`,
         };
     }
+    // If legacy fileInfo is available, return the necessary scripts to load legacy and polyfill files conditionally
     return {
         preload: preloadHtml,
         css: cssHtml,
-        js: `<script type="module">const script=document.createElement("script");try {if(!"noModule" in HTMLScriptElement.prototype)throw "";import.meta.url;import("_").catch(()=>1);(async function*(){})().next();script.type = "module";script.src="${publicPath}${pageInfo.file}";document.body.appendChild(script);window._isRunManifestJs = true;}catch(error) {}</script><script>window.onload=function(){if(window._isRunManifestJs) return;const script = document.createElement("script");script.src = "${publicPath}${legacyPolyfillsInfo.file}",script.onload= unction(){System.import("${publicPath}${legacyPageInfo.file}");},document.body.appendChild(script);}</script>`,
+        js: `<script type="module">const script=document.createElement("script");try {if(!"noModule" in HTMLScriptElement.prototype) throw "";import.meta.url;import("_").catch(()=>1);(async function*(){})().next();script.type = "module";script.src="${publicPath}${pageInfo.file}";document.body.appendChild(script);window._isRunManifestJs = true;}catch(error) {}</script><script>window.onload=function(){if(window._isRunManifestJs) return;const script = document.createElement("script");script.src = "${publicPath}${legacyPolyfillsInfo.file}",script.onload=function(){System.import("${publicPath}${legacyPageInfo.file}");},document.body.appendChild(script);}</script>`,
     };
 };
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -16,7 +16,7 @@ describe("parser manifest.json with error", () => {
 
         await expect(parser({ input: "/test/index.ts", outDir, publicPath, isDev: false }))
             .rejects
-            .toThrow("manifest.json文件中未发现入口");
+            .toThrow("Entry not found in the manifest.json file");
     });
 
     // 清理 mock
@@ -38,7 +38,7 @@ describe("parser in production environment", () => {
     it("should throw an error with invalid input", async () => {
         await expect(parser({ input: "", outDir, publicPath, isDev: false }))
             .rejects
-            .toThrow("参数有误");
+            .toThrow("Invalid parameters");
     });
 
     // 清理 mock
@@ -52,7 +52,7 @@ describe("parser in production environment with legacy support", () => {
     it("should return correct legacy script tags when legacy entries are present", async () => {
         const result = await parser({ input, outDir: path.resolve(__dirname, "./fixtures/dist-legacy"), publicPath, isDev: false });
         // 检查是否包含legacy脚本和polyfill
-        expect(result.js).toEqual("<script type=\"module\">const script=document.createElement(\"script\");try {if(!\"noModule\" in HTMLScriptElement.prototype)throw \"\";import.meta.url;import(\"_\").catch(()=>1);(async function*(){})().next();script.type = \"module\";script.src=\"/public/dist/assets/main.123abc.js\";document.body.appendChild(script);window._isRunManifestJs = true;}catch(error) {}</script><script>window.onload=function(){if(window._isRunManifestJs) return;const script = document.createElement(\"script\");script.src = \"/public/dist/assets/legacy-polyfills.ghi789.js\",script.onload= unction(){System.import(\"/public/dist/assets/main-legacy.abc123.js\");},document.body.appendChild(script);}</script>");
+        expect(result.js).toEqual("<script type=\"module\">const script=document.createElement(\"script\");try {if(!\"noModule\" in HTMLScriptElement.prototype) throw \"\";import.meta.url;import(\"_\").catch(()=>1);(async function*(){})().next();script.type = \"module\";script.src=\"/public/dist/assets/main.123abc.js\";document.body.appendChild(script);window._isRunManifestJs = true;}catch(error) {}</script><script>window.onload=function(){if(window._isRunManifestJs) return;const script = document.createElement(\"script\");script.src = \"/public/dist/assets/legacy-polyfills.ghi789.js\",script.onload=function(){System.import(\"/public/dist/assets/main-legacy.abc123.js\");},document.body.appendChild(script);}</script>");
     });
 
     // 清理 mock
